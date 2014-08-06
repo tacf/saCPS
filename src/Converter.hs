75c3a9f4427e4686704b0934d9311c1a155d4796
tacf <dev.tiago@gmail.com>
2014-08-06 20:22:43 +0100

    Fixed error conversion functions (vec -> (maps _ vec))

--- src/Converter.hs
+++ src/Converter.hs
@@ -15,7 +15,7 @@ la2cpsS :: S -> M
 la2cpsS s = case s of
  (RET e)         -> LRET (la2cpsE e)
  (ATRB l e s)    -> BNDR l (la2cpsE e) (la2cpsS s)
- (INV l f vec s) -> LINV l f vec (la2cpsS s)
+ (INV l f vec s) -> LINV l f (map la2cpsE vec) (la2cpsS s)
  (IF e s1 s2)    -> LIF (la2cpsE e) (la2cpsS s1) (la2cpsS s2) 
 
 la2cpsE :: E -> B
@@ -32,7 +32,7 @@ cps2laM :: M -> S
 cps2laM m = case m of
  (LRET b)         -> RET (cps2laB b)
  (BNDR l b m)     -> ATRB l (cps2laB b) (cps2laM m)
- (LINV l f vec m) -> INV l f vec (cps2laM m) 
+ (LINV l f vec m) -> INV l f (map cps2laB vec) (cps2laM m) 
  (LIF b m1 m2)    -> IF (cps2laB b) (cps2laM m1) (cps2laM m2)
 
 cps2laB :: B -> E
