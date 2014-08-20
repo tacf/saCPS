86d6e574582d0e70c3532c2fb640586b26d88e03
tacf <dev.tiago@gmail.com>
2014-08-20 15:45:31 +0100

    parserM remade

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,9 +4,9 @@ import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Token
 import Languages
 import Converter
-                   
+
 laDef :: LanguageDef st
-laDef = LanguageDef 
+laDef = LanguageDef
            { commentStart   = "/*"
            , commentEnd     = "*/"
            , commentLine    = "//"
@@ -21,7 +21,7 @@ laDef = LanguageDef
            }
 
 cpsDef :: LanguageDef st
-cpsDef = LanguageDef 
+cpsDef = LanguageDef
            {commentStart   = "/*"
            , commentEnd     = "*/"
            , commentLine    = "//"
@@ -34,7 +34,7 @@ cpsDef = LanguageDef
            , reservedNames  = ["k.k","if"]
            , caseSensitive  = True
            }
-           
+
 
 la :: TokenParser st
 la = makeTokenParser laDef
@@ -54,33 +54,31 @@ parseVector c p = do { l <- sepBy p (skipMany1 (space <|> char c))
 
 parseE :: Parser E
 parseE = do { c <- number
-            ; return $ CNST c 
-            } 
+            ; return $ CNST c
+            }
 	 <|> do { l <- (identifier la)
             ; return $ VAR l
-            } 
-           
+            }
+
+
 parseB :: Parser B
-parseB = do { (reservedOp cps) "\\"
-            ; (reserved cps) "k.k"
-            ; do { c <- number
-                 ; return $ LCNST c 
-                 } 
-              <|> 
-              do { l <- (identifier cps)
-                 ; return $ ID l 
-                 }
+parseB = do { c <- number
+            ; return $ LCNST c
+            }
+         <|>
+         do { l <- (identifier cps)
+            ; return $ ID l
             }
 
 parseInv :: Parser (Label,[E])
-parseInv = do { l <- (identifier la) 
+parseInv = do { l <- (identifier la)
               ; vec <- (parens la) (parseVector ',' parseE)
               ; return $ (l,vec)
               }
-              
+
 parseLInv :: Parser (Label,[B])
-parseLInv = do { l <- (identifier cps) 
-               ; vec <- parseVector ' ' parseB
+parseLInv = do { l <- (identifier cps)
+               ; vec <- parseVector ' ' (do{(reservedOp cps)"\\";(reserved cps) "k.k" ;parseB})
                ; return $ (l,vec)
                }
 
@@ -89,7 +87,7 @@ parseS = do { (reserved la) "ret"
             ; e <- (parens la) parseE
             ; return $ RET e
             }
-         <|> 
+         <|>
          do { (reserved la) "if"
             ; e <- (parens la) parseE
             ; s0 <- (braces la) parseS
@@ -117,45 +115,58 @@ parseS = do { (reserved la) "ret"
             ; return exp
             }
 
+
 parseM :: Parser M
-parseM = do{ fi <- try ((parens cps) 
-                (do { (reserved cps) "if"
-                    ; b  <- (parens cps) parseB
-                    ; m0 <- parseM
-                    ; m1 <- parseM
-                    ; return $ LIF b m0 m1
-                    }))
-            ; return fi}
-         <|>
-         do { (parens cps) ( 
-                do { (reservedOp cps) "\\"
-                   ; (reserved cps) "k.k"
-                   ; e <- (parens cps) parseB
-                   ; return $ LRET e
-                   }
-              )
-            }
-         <|>
-         do { b <- try (
-                do { b <- (parens cps) parseB
-                   ; bndr <- 
-                     (parens cps) (do { x <- (identifier cps)
-                                     ; m <- parseM
-                                     ; return $ BNDR x b m
-                                     })
-                   ; return bndr
-                   } )
-                <|>
-                do { (i,l) <- (parens cps) parseLInv
-                   ; inv <-
-                        (parens cps) (do { x <- (identifier cps)
-                                         ; m <- parseM
-                                         ; return $ LINV x i l m
-                                         })
-                   ; return inv
-                   }
-            ; return b
-            }
+parseM = do { char '('
+             ; m <-
+                  try(
+                  do { (reserved cps) "if"
+                     ; b  <- (parens cps) (do{(reservedOp cps) "\\"; (reserved cps) "k.k" ; parseB })
+                     ; m0 <- parseM
+                     ; m1 <- parseM
+                     ; char ')'
+                     ; return $ LIF b m0 m1
+                     }
+                     )
+                  <|>
+                  do { (reservedOp cps) "\\"
+                     ; (reserved cps) "k.k"
+                     ;  retOrBndr <- ((parens cps)
+                                            (do{ (reservedOp cps) "\\"
+                                               ; (reserved cps) "k.k"
+                                               ; b <- parseB
+                                               ; char ')'
+                                               ; return $ LRET b
+                                               })
+                                        )
+                                    <|>
+                                    do { b <- parseB
+                                       ; char ')'
+                                       ; r <- (parens cps)
+                                               (do { (reservedOp cps) "\\"
+                                                   ; x <- (identifier cps)
+                                                   ; char '.'
+                                                   ; m <- parseM
+                                                   ; return $ BNDR x b m
+                                                   })
+
+                                       ; return r
+                                       }
+                    ; return retOrBndr
+                    }
+                  <|>
+                  do { (i,l) <- parseLInv
+                     ; char ')'
+                     ; inv <-
+                       (parens cps) (do { x <- (identifier cps)
+                                        ; m <- parseM
+                                        ; return $ LINV x i l m
+                                        })
+                     ; return inv
+                     }
+             ; return m
+             }
+
 
 parseProc :: Parser Proc
 parseProc = do { l <- (identifier la)
@@ -177,10 +188,10 @@ parseLProc = do { l <- (identifier cps)
                        ; body <- parseM
                        ; return $ LPROC l vec body
                        })
-               ; return proc 
+               ; return proc
                }
-               
-               
+
+
 parseProgram :: Parser Program
 parseProgram = do { l <- sepBy parseProc (skipMany space)
                  ; return l
@@ -190,6 +201,4 @@ parseLProgram :: Parser LProgram
 parseLProgram = do { l <- sepBy parseLProc (skipMany space)
                  ; return l
                  }
-
-
 
