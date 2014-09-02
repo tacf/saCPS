8c25b0e5adb1b0527a07b02b4ae3df3c1aabde27
tacf <dev.tiago@gmail.com>
2014-09-02 14:58:40 +0100

    Bug fix: parseM + parseLInv

--- src/Parser.hs
+++ src/Parser.hs
@@ -78,7 +78,7 @@ parseInv = do { l <- (identifier la)
 
 parseLInv :: Parser (Label,[B])
 parseLInv = do { l <- (identifier cps)
-               ; vec <- parseVector ' ' (do{(reservedOp cps)"\\";(reserved cps) "k.k" ;parseB})
+               ; vec <- parseVector ' ' ((parens cps) (do{(reservedOp cps)"\\";(reserved cps) "k.k" ;parseB}))
                ; return $ (l,vec)
                }
 
@@ -158,7 +158,9 @@ parseM = do { char '('
                   do { (i,l) <- parseLInv
                      ; char ')'
                      ; inv <-
-                       (parens cps) (do { x <- (identifier cps)
+                       (parens cps) (do { (reservedOp cps) "\\"
+                                        ; x <- (identifier cps)
+                                        ; char '.'
                                         ; m <- parseM
                                         ; return $ LINV x i l m
                                         })
@@ -202,3 +204,11 @@ parseLProgram = do { l <- sepBy parseLProc (skipMany space)
                  ; return l
                  }
 
+
+readFromFile p s = do
+                x <- parseFromFile p s
+                case x of
+                    (Right x) -> return x
+                    (Left e) -> do 
+                                putStrLn (show e)
+                                return []
